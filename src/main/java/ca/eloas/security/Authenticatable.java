c4452767985c9e17faecc1390a3be1a45624753e
Jean-Philippe Belanger <jpbelang@gmail.com>
2013-09-25 08:25:23 -0400

    Split commons into different projects.

--- src/main/java/ca/eloas/security/Authenticatable.java
+++
@@ -1,9 +0,0 @@
-package ca.eloas.security;
-
-/**
- * @author JP
- */
-public interface Authenticatable {
-
-    AuthInformation authenticate() throws AuthenticationException;
-}
--- src/main/java/ca/eloas/security/OwnedResource.java
+++
@@ -1,9 +0,0 @@
-package ca.eloas.security;
-
-/**
- * @author JP
- */
-public interface OwnedResource {
-
-    boolean isOwner(AuthInformation info);
-}
